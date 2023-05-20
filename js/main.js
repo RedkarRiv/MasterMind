23bf243fd566ff4389f11ffaa9b3e16ae6e4c839
RedkarRiv <128104355+RedkarRiv@users.noreply.github.com>
2023-05-20 18:29:18 +0200

    FIX: Correccion de SessionStorage. Script para traer los datos a la partida y codificacion de los mismos: OK

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let A = sessionStorage.getItem("ColorSelected1");
+let B = sessionStorage.getItem("ColorSelected2");
+let C = sessionStorage.getItem("ColorSelected3");
+let D = sessionStorage.getItem("ColorSelected4");
+let jugador = sessionStorage.getItem("usuario");
+
+
+console.log(A);
+console.log(B);
+console.log(C);
+console.log(D);
+console.log(jugador);
