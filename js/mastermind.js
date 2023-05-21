828f56fb82bf1b5dda89f9bee3433680f2723ea4
RedkarRiv <128104355+RedkarRiv@users.noreply.github.com>
2023-05-21 18:07:56 +0200

    UPDATE: Incluido valor por defecto a NOMBRE por si el ususario no quiere introducir nada

--- js/mastermind.js
+++ js/mastermind.js
@@ -1,9 +1,9 @@
-let A = sessionStorage.getItem("ColorSelected1");
-let B = sessionStorage.getItem("ColorSelected2");
-let C = sessionStorage.getItem("ColorSelected3");
-let D = sessionStorage.getItem("ColorSelected4");
-let E = sessionStorage.getItem("ColorSelected5");
-let F = sessionStorage.getItem("ColorSelected6");
+let A = sessionStorage.getItem("ColorSelected1") ?? "#fc0303";
+let B = sessionStorage.getItem("ColorSelected2") ?? "#0602ed";
+let C = sessionStorage.getItem("ColorSelected3") ?? "#3ff90b";
+let D = sessionStorage.getItem("ColorSelected4") ?? "#c240d4";
+let E = sessionStorage.getItem("ColorSelected5") ?? "#04f1ed";
+let F = sessionStorage.getItem("ColorSelected6") ?? "#dc6f09";
 
 
 let lvlMode = sessionStorage.getItem("nivelSeleccionado");
@@ -11,11 +11,11 @@ let jugador = sessionStorage.getItem("usuario");
 const arrayCode = (lvlMode === "easy") ? [A, B, C, D] : (lvlMode === "medium") ? [A, B, C, D, E] : [A, B, C, D, E, F];
 
 
-// console.log(A);
-// console.log(B);
-// console.log(C);
-// console.log(D);
-// console.log(jugador);
+console.log(A);
+console.log(B);
+console.log(C);
+console.log(D);
+console.log(jugador);
 
 // SECRET CODE GENERATOR WITH ARRAY
 
@@ -34,11 +34,14 @@ console.log(secretCode);
 
 // SECRET CODE CONVERTED IN SIMPLE CODE STRING AND WINNER CODE STRING
 
-const Code1 = secretCode[0].toString("");
-const Code2 = secretCode[1].toString("");
-const Code3 = secretCode[2].toString("");
-const Code4 = secretCode[3].toString("");
+const Code1 = secretCode[0].toString();
+const Code2 = secretCode[1].toString();
+const Code3 = secretCode[2].toString();
+const Code4 = secretCode[3].toString();
 const winnerCode = Code1 + Code2 + Code3 + Code4;
+
+
+
 
 console.log(Code1);
 console.log(Code2);
--- js/playerselect.js
+++ js/playerselect.js
@@ -1,9 +1,11 @@
 const nameSave = () => {
-
     let valor = document.getElementById("inputName").value;
+    if (!valor) {
+        valor = "El/La jugador/a";
+    }
+    console.log(valor);
     sessionStorage.setItem("usuario", valor);
     window.location.href = "./levelselect.html";
-}
-
+};
 
 
