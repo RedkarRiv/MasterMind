c928f9dcea2eb14fc562ee0e161200bd19f05552
RedkarRiv <128104355+RedkarRiv@users.noreply.github.com>
2023-05-28 19:17:45 +0200

    FIX: Ajuste de elementos en version movil para la pagina index.html

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const colors = ['#fb0303', '#c503fb', '#0b03fb', '#03fb28', '#d6fb03', '#fb9c03'];
+let currentIndex = 0;
+const lootText = document.getElementById('gameName');
+
+const changeColors = () => {
+    const color = colors[currentIndex % colors.length];
+    lootText.style.color = color;
+    currentIndex++;
+};
+
+// Llamada inicial a la función changeColors()
+changeColors();
+
+// Temporizador para cambiar de color cada 3 segundos (3000 milisegundos)
+setInterval(changeColors, 2000);
